f9ac71f87fdeec15ba14f463770bd4c4ff2cadb5
Everth Paredes <epe0413@gmail.com>
2024-04-04 14:57:02 -0600

    Writing in the State

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,6 +9,13 @@ export default function Form() {
         calories: 0
     });
 
+    const handleChange = (e) => {
+        setActivity({
+            ...activity,
+            [e.target.id]: e.target.value
+        })
+    }
+
     return (
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className="grid grid-cols-1 gap-3">
@@ -18,6 +25,7 @@ export default function Form() {
                     id="category"
                     className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                     value={activity.category}
+                    onChange={handleChange}
                 >
                     {categories.map(category => (
                         <option key={category.id} value={category.id}>{category.name}</option>
@@ -33,6 +41,7 @@ export default function Form() {
                     className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                     placeholder="Ej. Comida, Jugo de Naranja, Ensalda, Ejercicio, Pesas, Biblioteca"
                     value={activity.name}
+                    onChange={handleChange}
                 />
             </div>
             <div className="grid grid-cols-1 gap-3">
@@ -43,6 +52,7 @@ export default function Form() {
                     className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                     placeholder="Calorias, ejemplo: 400, 500"
                     value={activity.calories}
+                    onChange={handleChange}
                 />
             </div>
 
