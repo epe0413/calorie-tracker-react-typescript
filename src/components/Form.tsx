5f51009f797dca44566a6fe2895b932ad4f9bd7b
Everth Paredes <epe0413@gmail.com>
2024-04-09 09:32:51 -0600

    Conditionally display Text if it is Exercise or Food on save

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -19,6 +19,11 @@ export default function Form() {
         })
     }
 
+    const isValidActivity = () => {
+        const {name, calories} = activity
+        return name.trim() !== '' && calories > 0
+    }
+
     return (
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className="grid grid-cols-1 gap-3">
@@ -61,8 +66,9 @@ export default function Form() {
 
             <input
                 type="submit"
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold text-white cursor-pointer rounded"
-                value='Guardar Comida o Ejercicio'
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold text-white cursor-pointer rounded disabled:opacity-10"
+                value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+                disabled= {!isValidActivity()}
             />
         </form>
     )
