25a2faef05eb9c2e9ec9d2e477012e5d5a467cc0
Everth Paredes <epe0413@gmail.com>
2024-04-15 13:55:43 -0600

    Creating functionality to restart the app

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -29,7 +29,7 @@ export default function ActivityList({activities, dispatch}: ActivityListProps) 
             <p className='text-center my-5'>No hay actividades aún...</p>
             :
             activities.map( activity => (
-                <div key={activity.id} className='px-5 py-10 bg-white mt-5 flex justify-between rounded-lg'>
+                <div key={activity.id} className='px-5 py-10 bg-white mt-5 flex justify-between rounded-lg shadow-lg'>
                     <div className='space-y-2 relative'>
                         <p className={`absolute -top-8 -left-8 px-10 py-2 text-white font-bold rounded-md ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
                             {categoryName(+activity.category)}
