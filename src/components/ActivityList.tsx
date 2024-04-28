5f193377d2976e155893b89e1740fcc2eab9e1e2
Everth Paredes <epe0413@gmail.com>
2024-04-27 21:27:21 -0600

    useContext

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,23 +1,9 @@
-import { useMemo, Dispatch } from 'react';
-import { Activity } from '../types/index';
-import { categories } from '../data/categories';
 import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline'
-import { ActivityActions } from '../reducers/activity-reducer';
+import { useActivity } from '../hooks/useActivity';
 
-type ActivityListProps = {
-    activities: Activity[],
-    dispatch: Dispatch<ActivityActions>
+export default function ActivityList() {
 
-}
-
-export default function ActivityList({activities, dispatch}: ActivityListProps) {
-
-    const categoryName = useMemo(() =>
-        (category: Activity['category']) =>
-        categories.map( cat => cat.id === category ? cat.name : '')
-        ,[activities])
-    
-    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
+    const {state, dispatch, isEmptyActivities, categoryName} = useActivity();
 
     return (
         <>
@@ -28,7 +14,7 @@ export default function ActivityList({activities, dispatch}: ActivityListProps) 
             { isEmptyActivities ? 
             <p className='text-center my-5'>No hay actividades aún...</p>
             :
-            activities.map( activity => (
+            state.activities.map( activity => (
                 <div key={activity.id} className='px-5 py-10 bg-white mt-5 flex justify-between rounded-lg shadow-lg'>
                     <div className='space-y-2 relative'>
                         <p className={`absolute -top-8 -left-8 px-10 py-2 text-white font-bold rounded-md ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
